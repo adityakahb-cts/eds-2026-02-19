30fa3d043600d32200e99d3c99c0f300979ed127
Aditya Bhakay <aditya.bhakay@cognizant.com>
2026-04-15 11:16:47 +0530

    initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,39 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import globals from 'globals';
+import babelParser from '@babel/eslint-parser';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+});
+
+export default [
+  {
+    ignores: ['helix-importer-ui/**', '**/*.min.js', 'eslint.config.mjs'],
+  },
+  ...compat.extends('airbnb-base'),
+  {
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        allowImportExportEverywhere: true,
+        sourceType: 'module',
+        requireConfigFile: false,
+      },
+      globals: {
+        ...globals.browser,
+      },
+    },
+    rules: {
+      'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
+      'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
+      'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    },
+  },
+];
